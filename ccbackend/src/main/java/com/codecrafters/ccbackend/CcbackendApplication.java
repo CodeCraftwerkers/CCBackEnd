f7a3cf1413dafef28700bc4d0422c3a9fb1bec24
DevErika <erikamontoya2023@gmail.com>
2025-10-27 10:08:28 +0100

    first commit

---
+++ ccbackend/src/main/java/com/codecrafters/ccbackend/CcbackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecrafters.ccbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcbackendApplication.class, args);
+	}
+
+}
---
+++ ccbackend/src/test/java/com/codecrafters/ccbackend/CcbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrafters.ccbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
