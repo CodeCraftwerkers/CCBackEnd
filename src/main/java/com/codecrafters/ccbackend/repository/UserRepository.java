5410ba82d62dc022b843dce78d12f31980f5401b
surayac <surayacsm@gmail.com>
2025-10-31 14:26:07 +0100

    refactor(userRepository): add optional

--- src/main/java/com/codecrafters/ccbackend/repository/UserRepository.java
+++ src/main/java/com/codecrafters/ccbackend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.codecrafters.ccbackend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.codecrafters.ccbackend.entity.User;
 
 public interface UserRepository extends JpaRepository <User, Long> {
-    
+    Optional<User> findByEmail(String email);
 }
