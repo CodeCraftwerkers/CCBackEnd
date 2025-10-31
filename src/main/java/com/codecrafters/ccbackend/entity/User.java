f3a254a1a489529d60ebb32fadb460ec4831fb84
surayac <surayacsm@gmail.com>
2025-10-31 14:50:54 +0100

    feat(event_signups): add relational table between users and events

--- src/main/java/com/codecrafters/ccbackend/entity/User.java
+++ src/main/java/com/codecrafters/ccbackend/entity/User.java
@@ -1,10 +1,16 @@
 package com.codecrafters.ccbackend.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.AllArgsConstructor;
@@ -31,5 +37,14 @@ public class User {
     private String email;
 
     private String password;
+
+    @ManyToMany
+    @JoinTable(
+        name = "event_signups",
+        joinColumns = @JoinColumn(name="user_id"),
+        inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    @Builder.Default
+    private Set<Event> signedUpEvents = new HashSet<>();
     
 }
