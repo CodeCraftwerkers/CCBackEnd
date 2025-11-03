8191434767b18abebb68649cb3e7ea5686205abf
Daniella Pacheco <daniellak.pacheco@gmail.com>
2025-11-03 15:50:10 +0100

    fix(Mapper): fix MapStruct annotation processing and mapper warnings

--- src/main/java/com/codecrafters/ccbackend/mapper/UserMapper.java
+++ src/main/java/com/codecrafters/ccbackend/mapper/UserMapper.java
@@ -5,16 +5,15 @@ import com.codecrafters.ccbackend.dto.response.UserResponseDTO;
 import com.codecrafters.ccbackend.entity.User;
 import org.mapstruct.*;
 
-
-@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true), unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
-    
+
     @Mapping(target = "id", ignore = true)
-    User toEntity(UserRequestDTO request);    
-    
+    User toEntity(UserRequestDTO request);
+
     UserResponseDTO toResponse(User user);
-    
-    @Mapping(target = "id", ignore = true)
+
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
     void updateEntityFromRequest(UserRequestDTO request, @MappingTarget User user);
 }
